3d329401fb68b1db3ea7eb28caaa5b5db4ddc85d
joaosousa <joao.sousa@corebiz.ag>
2025-10-29 20:29:38 -0300

    feat: add initial AddTaskDialog component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -9,12 +9,14 @@ import {
   TrashIcon,
 } from '../assets/icons'
 import TASKS from '../constants/tasks'
+import AddTaskDialog from './AddTaskDialog'
 import Button from './Button'
 import TaskItem from './TaskItem'
 import TasksSeparator from './TasksSeparator'
 
 const Tasks = () => {
   const [tasks, setTasks] = useState(TASKS)
+  const [addTasksDialogIsOpen, setAddTasksDialogIsOpen] = useState(false)
 
   const morningTasks = tasks.filter((task) => task.time === 'morning')
   const afternoonTasks = tasks.filter((task) => task.time === 'afternoon')
@@ -60,12 +62,14 @@ const Tasks = () => {
             Limpar tarefas
             <TrashIcon />
           </Button>
-          <Button>
+          <Button onClick={() => setAddTasksDialogIsOpen(true)}>
             Adicionar tarefa
             <AddIcon />
           </Button>
         </div>
       </div>
+
+      <AddTaskDialog isOpen={addTasksDialogIsOpen} />
 
       <div className="gap-6 space-y-6 rounded-xl bg-white p-6">
         <div className="space-y-3">
